b6c404a80a73af63cb801dd6a3f00a776e0085f9
Luis Ramirez <me.luisramirez@gmail.com>
2022-09-27 23:06:19 +0200

    feat(question-timer): add question-timer component

---
+++ src/app/components/question-timer/question-timer.component.ts
@@ -0,0 +1,78 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'qz-question-timer',
+  templateUrl: './question-timer.component.html',
+  styleUrls: ['./question-timer.component.scss'],
+})
+export class QzQuestionTimerComponent implements OnInit {
+  @Input()
+  limitSeconds: number = 120;
+
+  @Input()
+  isMinutesFormat!: boolean;
+  @Output() emitEndTimer: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  timeLeft!: number;
+  interval!: ReturnType<typeof setInterval>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.generateTimer();
+  }
+
+  /**
+   * Generate timer witn setInterval. When timer is finished emit with EventEmitter, for the parent component
+   *
+   * @memberof QzQuestionTimer
+   */
+  generateTimer() {
+    this.timeLeft = this.limitSeconds;
+
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      }
+
+      if (this.timeLeft === 0) {
+        this.emitEndTimer.emit(true);
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  /**
+   * Format seconds, if user use the prop 'isMinutesFormat" the timer formated to mm:ss, if is false the format on seconds
+   *
+   * @param {number} seconds
+   * @return {*}
+   * @memberof QzQuestionTimer
+   */
+  formatSeconds(seconds: number) {
+    const minutesTotal = Math.floor((seconds % 3600) / 60)
+      .toString()
+      .padStart(2, '0');
+
+    const secondsTotal = Math.floor(seconds % 60)
+      .toString()
+      .padStart(2, '0');
+
+    this.isMinutesFormat;
+
+    if (this.isMinutesFormat) {
+      return `${minutesTotal}:${secondsTotal}`;
+    }
+
+    return `${seconds}s`;
+  }
+
+  /**
+   * Restar timer
+   *
+   * @memberof QzQuestionTimer
+   */
+  restarTimer(): void {
+    this.timeLeft = this.limitSeconds;
+  }
+}
